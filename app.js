b8bccae96683b69a0d7dc13fadef8f1c9ef2aabe
Nate <yolles@adobe.com>
2024-08-04 15:17:52 -0600

    child process

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const config = require('./config.json');
 const bodyParser = require('body-parser');
-const VLC = require('vlc-client');
+//const VLC = require('vlc-client');
+const childProcess = require('child_process');
 
 const app = express();
 const port = config.portNumber;
-const vlc = new VLC.Client({
-    ip: 'localhost',
-    port: 8080,
-    username: 'admin',
-    password: 'admin'
-});
+// const vlc = new VLC.Client({
+//     ip: 'localhost',
+//     port: 8080,
+//     username: 'admin',
+//     password: 'admin'
+// });
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +22,12 @@ app.get('/videos/list', (req, res) => {
 });
 
 app.post('/videos/play/:videoId', (req, res) => {
-    vlc.playFile(config.videos[req.params.videoId].path);
+    // command line works:
+    // vlc videos/Slender\ Specter_Startle\ Scare_Win_V.mp4 --no-video-title-show --fullscreen
+    //vlc.playFile(config.videos[req.params.videoId].path);
+    childProcess.exec('vlc videos/Slender\ Specter_Startle\ Scare_Win_V.mp4 --no-video-title-show --fullscreen', (msg) => {
+        console.log(msg);
+    });
     res.send(`Got a POST request with video ID: ${config.videos[req.params.videoId].path}`);
 })
 
